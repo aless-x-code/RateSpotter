4f78cbd18bbbaf5c33fa5990b6b07b6d56923fde
“alessandro <aless.cam.h@gmail.com>
2024-06-20 11:24:25 -0400

    decoupled main.py with routes.py

--- main.py
+++ main.py
@@ -9,19 +9,11 @@ from flask_login import LoginManager, UserMixin #type: ignore
 from pymongo.mongo_client import MongoClient #type: ignore
 from pymongo.server_api import ServerApi #type: ignore
 import certifi
+from routes import init_login_manager
+from routes import movies_api_v1
+
 
 load_dotenv()
-
-mongo_db_pw = os.environ.get("mongo_db_pw")
-mongo_user = os.environ.get("mongo_user")
-
-uri = f"mongodb+srv://{mongo_user}:{mongo_db_pw}@test-db.rzdnkbr.mongodb.net/?retryWrites=true&w=majority&appName=test-db"
-client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
-db = client[os.environ.get("db_name")] 
-users_collection = db[os.environ.get("collection")]
-
-
-
 
 current_file = Path(__file__).resolve()
 app_dir = current_file.parent
@@ -31,72 +23,8 @@ template_folder = app_dir / "templates"
 app = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY") or secrets.token_hex(32)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-class User(UserMixin):
-    def __init__(self, username):
-        self.username = username
-
-    def get_id(self):
-        return self.username
-
-@login_manager.user_loader
-def load_user(username):
-    user = users_collection.find_one({"username": username})
-    if user:
-        return User(username=user["username"])
-    return None
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        hashed_password = generate_password_hash(password)
-
-        if users_collection.find_one({'username': username}):
-            flash('Username already exists. Choose a different one.', 'danger')
-        else:
-            users_collection.insert_one({'username': username, 'password': hashed_password})
-            flash('Registration successful. You can now log in.', 'success')
-            return redirect(url_for('login'))
-
-    return render_template('register.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = users_collection.find_one({'username': username})
-
-        if user and check_password_hash(user['password'], password):
-            user_obj = User(username=user['username'])
-            login_user(user_obj)
-            flash('Login successful.', 'success')
-            return redirect(url_for('user_homepage'))
-        else:
-            flash('Invalid username or password. Please try again.', 'danger')
-
-    return render_template('login.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('You have been logged out.', 'success')
-    return redirect(url_for('login'))
-
-@app.route('/user_homepage')
-@login_required
-def user_homepage():
-    return render_template("user_homepage.html", username=current_user.username)
+app.register_blueprint(movies_api_v1)
+init_login_manager(app)
 
 
 if __name__ == "__main__":
--- routes.py
+++ routes.py
@@ -1,56 +1,93 @@
-# from flask import Blueprint
-# from auth import User, users_collection
-# from flask_login import login_user, login_required, logout_user, current_user
-# from werkzeug.security import generate_password_hash, check_password_hash
-# from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+from pathlib import Path
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
+from dotenv import load_dotenv
+import secrets
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin #type: ignore
+from pymongo.mongo_client import MongoClient #type: ignore
+from pymongo.server_api import ServerApi #type: ignore
+import certifi
 
-# movies_api_v1 = Blueprint("ratespotter_blueprint", __name__)
+def init_login_manager(app):
+    login_manager.init_app(app)
+    login_manager.login_view = "ratespotter_blueprint.login"
 
-# @movies_api_v1.route("/")
-# def index():
-#     return render_template("index.html")
+movies_api_v1 = Blueprint("ratespotter_blueprint", __name__)
 
-# @movies_api_v1.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         hashed_password = generate_password_hash(password)
+load_dotenv()
 
-#         if users_collection.find_one({'username': username}):
-#             flash('Username already exists. Choose a different one.', 'danger')
-#         else:
-#             users_collection.insert_one({'username': username, 'password': hashed_password})
-#             flash('Registration successful. You can now log in.', 'success')
-#             return redirect(url_for('ratespotter_blueprint.login'))
+mongo_db_pw = os.environ.get("mongo_db_pw")
+mongo_user = os.environ.get("mongo_user")
 
-#     return render_template('register.html')
+uri = f"mongodb+srv://{mongo_user}:{mongo_db_pw}@test-db.rzdnkbr.mongodb.net/?retryWrites=true&w=majority&appName=test-db"
+client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+db = client[os.environ.get("db_name")] 
+users_collection = db[os.environ.get("collection")]
 
-# @movies_api_v1.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         user = users_collection.find_one({'username': username})
+login_manager = LoginManager()
 
-#         if user and check_password_hash(user['password'], password):
-#             user_obj = User(username=user['username'])
-#             login_user(user_obj)
-#             flash('Login successful.', 'success')
-#             return redirect(url_for('ratespotter_blueprint.user_homepage'))
-#         else:
-#             flash('Invalid username or password. Please try again.', 'danger')
+class User(UserMixin):
+    def __init__(self, username):
+        self.username = username
 
-#     return render_template('login.html')
+    def get_id(self):
+        return self.username
+    
+@login_manager.user_loader
+def load_user(username):
+    user = users_collection.find_one({"username": username})
+    if user:
+        return User(username=user["username"])
+    return None
 
-# @movies_api_v1.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     flash('You have been logged out.', 'success')
-#     return redirect(url_for('ratespotter_blueprint.login'))
+@movies_api_v1.route("/")
+def index():
+    return render_template("index.html")
 
-# @movies_api_v1.route('/user_homepage')
-# @login_required
-# def user_homepage():
-#     return render_template("user_homepage.html", username=current_user.username)
+@movies_api_v1.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+
+        if users_collection.find_one({'username': username}):
+            flash('Username already exists. Choose a different one.', 'danger')
+        else:
+            users_collection.insert_one({'username': username, 'password': hashed_password})
+            flash('Registration successful. You can now log in.', 'success')
+            return redirect(url_for('ratespotter_blueprint.login'))
+
+    return render_template('register.html')
+
+@movies_api_v1.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = users_collection.find_one({'username': username})
+
+        if user and check_password_hash(user['password'], password):
+            user_obj = User(username=user['username'])
+            login_user(user_obj)
+            flash('Login successful.', 'success')
+            return redirect(url_for('ratespotter_blueprint.user_homepage'))
+        else:
+            flash('Invalid username or password. Please try again.', 'danger')
+
+    return render_template('login.html')
+
+@movies_api_v1.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('ratespotter_blueprint.login'))
+
+@movies_api_v1.route('/user_homepage')
+@login_required
+def user_homepage():
+    return render_template("user_homepage.html", username=current_user.username)
+
